2e8dc7a8b9ebf7531a726f2da3d6ff1c22d1bc3f
Jesus Enrique Nava Sanchez <jesus.nava0112@gmail.com>
2019-05-20 21:56:19 +0200

    affichage des lines

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ApiService } from '../services/api.service';
+import { LineToLineMappedSource } from 'webpack-sources';
 
 @Component({
   selector: 'app-tab2',
@@ -9,7 +10,22 @@ import { ApiService } from '../services/api.service';
 })
 export class Tab2Page implements OnInit{
   lines:JSON;
+  lines2:JSON;
+  public items: Array<{id:String, gtfsId:String, shortName:String, longName:String,
+                  color:String, textColor:String, mode:String, type:String, res:String}> = [];
+  public trams: Array<{id:String, gtfsId:String, shortName:String, longName:String,
+    color:String, textColor:String, mode:String, type:String, res:String}> = [];
+    public CHRONO: Array<{id:String, gtfsId:String, shortName:String, longName:String,
+      color:String, textColor:String, mode:String, type:String, res:String}> = [];
+      public PROXIMO: Array<{id:String, gtfsId:String, shortName:String, longName:String,
+        color:String, textColor:String, mode:String, type:String, res:String}> = [];
+        public FLEXO: Array<{id:String, gtfsId:String, shortName:String, longName:String,
+          color:String, textColor:String, mode:String, type:String, res:String}> = [];
+          public C38: Array<{id:String, gtfsId:String, shortName:String, longName:String,
+            color:String, textColor:String, mode:String, type:String, res:String}> = [];
+            
   nom;
+  cant: 0
 
   constructor(private apiService : ApiService){}
 
@@ -18,13 +34,54 @@ export class Tab2Page implements OnInit{
   }
   
   searchChange(){
+    
     this.apiService.getData(false,"lines").subscribe(line=>{
-      console.log(line)
-       line.forEach(element => {
-        if(element.type=="TRAM"){
-        this.lines = element
-    }
-    });
+      /*console.log(line.lenght)
+      for(var i =0; i<=10; i++){
+        var obj = line;
+        console.log(obj[i].type)
+      }*/
+      line.forEach(element => {
+        this.items.push({
+          id:element.id,
+          gtfsId:element.gtfsId,
+          shortName:element.shortName,
+          longName:element.longName,
+          color:element.color, 
+          textColor:element.textColor, 
+          mode:element.mode, 
+          type:element.type,
+          res:element.res
+        });//end items.push
+        
+      });//end line.foreach
+      this.items.forEach(line =>{
+        if(line.type=="TRAM"){
+          this.trams.push(line);//end items.push
+        }
+      })//end items.foreach
+      this.items.forEach(line =>{
+        if(line.type=="CHRONO"){
+          this.CHRONO.push(line);//end items.push
+        }
+      })//end items.foreach
+      this.items.forEach(line =>{
+        if(line.type=="PROXIMO"){
+          this.PROXIMO.push(line);//end items.push
+        }
+      })//end items.foreach
+      this.items.forEach(line =>{
+        if(line.type=="FLEXO"){
+          this.FLEXO.push(line);//end items.push
+        }
+      })//end items.foreach
+      this.items.forEach(line =>{
+        if(line.type=="C38"){
+          this.C38.push(line);//end items.push
+        }
+      })//end items.foreach
+      //console.log(this.items)
+      //this.lines = line
     })
   }
 
